d596ac58c42b01c0581359d3454e6ac96ee370c4
phanhuuquy <91709328+phanhuuquy@users.noreply.github.com>
2023-07-28 01:47:24 +0700

    a

---
+++ app/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Laravel\Jetstream\Jetstream;
+
+class CreateNewUser implements CreatesNewUsers
+{
+    use PasswordValidationRules;
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param  array  $input
+     * @return \App\Models\User
+     */
+    public function create(array $input)
+    {
+        Validator::make($input, [
+            'Ten_nha_hang' => ['required', 'string', 'max:255'], 
+            'Dia_chi' => ['required', 'string', 'max:255'], 
+            'SDT' => ['required', 'string', 'max:12'],
+            // 'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'Ten_dang_nhap' => ['required', 'string',  'max:255', 'unique:users'],
+            'password' => $this->passwordRules(),
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
+        ])->validate();
